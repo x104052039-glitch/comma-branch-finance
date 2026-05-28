6b492a2debff780649a7145d0c502921ebe985b7
x104052039-glitch <x104052039@gmail.com>
2026-05-28 22:13:36 +0800

    Update vite.config.ts

--- 分校每月支出控管系統/vite.config.ts
+++ 分校每月支出控管系統/vite.config.ts
@@ -3,11 +3,10 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
-    resolveId(id) {
+    resolveId(id: string) {
       if (id.startsWith('figma:asset/')) {
         const filename = id.replace('figma:asset/', '')
         return path.resolve(__dirname, 'src/assets', filename)
@@ -17,20 +16,19 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/comma-branch-finance/',
+
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
